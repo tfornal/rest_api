8e1cd547d99f9d1d61ae841202c7bbcaa2827e20
tfornal <tomasz.fornal6@gmail.com>
2023-05-09 21:54:30 +0200

    upgrade

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import uuid
 from flask import Flask, request
 from db import items, stores
 
@@ -6,26 +7,33 @@ app = Flask(__name__)
 
 @app.get("/store")
 def get_stores():
-    return {"stores": stores}
+    return {"stores": list(stores.values())}
 
 
 @app.post("/store")  # httop://127.0.0.1:5000/store
 def create_store():
-    request_data = request.get_json()
-    new_store = {"name": request_data["name"], "items": []}
-    stores.append(new_store)
-    return new_store, 201
+    store_data = request.get_json()
+    store_id = uuid.uid4().hex
+    new_store = {**store_data, "id": store_id}
+    stores[store_id] = store
+    return store, 201
 
 
-@app.post("/store/<string:name>/item")
-def create_item(name):
-    request_data = request.get_json()
-    for store in stores:
-        if store["name"] == name:
-            new_item = {"name": request_data["name"], "price": request_data["price"]}
-            store["items"].append(new_item)
-            return new_item, 201
-    return {"message": "store not found"}, 404
+@app.post("/item")
+def create_item():
+    item_data = request.get_json()
+    if item_data["store_id"] not in stores:
+        return {"message": "Store not found"}, 404
+
+    item_id = uuid.uid4().hex
+    item = {**item_data, "id": item_id}
+    items[item_id] = item
+    return item, 201
+
+
+@app.get("/item")
+def get_all_items():
+    return {"items": list(items.values())}
 
 
 @app.get("/store/<string:store_id>")
@@ -36,9 +44,9 @@ def get_store(name):
         return {"message": "Store not found"}, 404
 
 
-@app.get("/store/<string:name>/item")
-def get_item(name):
-    for store in stores:
-        if store["name"] == name:
-            return {"items": store["items"]}
-    return {"message": "Store not found"}, 404
+@app.get("/item/<string:item_id>")
+def get_item(item_id):
+    try:
+        return items[item_id]
+    except KeyError:
+        return {"message": "Item not found"}, 404
